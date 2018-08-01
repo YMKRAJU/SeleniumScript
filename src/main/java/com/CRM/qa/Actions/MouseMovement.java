e8409f6b1a89a024b0b0005296fd7dbdf5a6270e
YMKRAJU <kanta2121@gmail.com>
2018-08-01 12:37:18 +0530

    Mouse Movement is created and Modified

--- src/main/java/com/CRM/qa/Actions/MouseMovement.java
+++ src/main/java/com/CRM/qa/Actions/MouseMovement.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import com.CRM.qa.Base.Click_ON;
-
-public class Mouse_Movement {
+public class MouseMovement {
 
 	public static void main(String[] args) throws InterruptedException {
 		String path = "C:\\Selenium3\\Drivers//geckodriver.exe";
@@ -18,15 +16,13 @@ public class Mouse_Movement {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-		driver.get("https://www.freecrm.com/");
-		driver.findElement(By.name("username")).sendKeys("YMKRAJU");
-		driver.findElement(By.name("password")).sendKeys("RanaRaghuRaju7");
-		Click_ON.clickon(driver, driver.findElement(By.xpath("//input[@type='submit'][@value='Login']")), 20);
+		driver.get("http://www.spicejet.com/");
 		Actions action = new Actions(driver);
-		action.moveToElement(driver.findElement(By.linkText("https://www.freecrm.com/system/index.cfm?action=contact"))).build().perform();
+		action.moveToElement(driver.findElement(By.xpath("//a[@id='highlight-addons']"))).build().perform();
 		Thread.sleep(3000);
-		action.moveToElement(driver.findElement(By.xpath("//a[text()='Contacts']"))).build().perform();
+		action.moveToElement(driver.findElement(By.xpath("//a[text()='Friends and Family']"))).build().perform();
 		Thread.sleep(3000);
-		action.moveToElement(driver.findElement(By.xpath("//a[text()='Contacts']//following::a[3]"))).click();
+
 	}
+
 }
